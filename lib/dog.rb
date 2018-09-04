5200bc7add8c203254d2b52584718961fc4e8c2f
Veniel Dorestave <veniel@noukod.com>
2018-09-04 19:19:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,18 @@
-class Person
-def initialize(name)
-  @name = name
-end
-end
+class Dog
+  def initialize (name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name=(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end 
+end 
